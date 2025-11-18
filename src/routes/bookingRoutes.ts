e9b152fc41c6f061be34d86d2cf278501db60382
shammu shaik <skshakilask67@gmail.com>
2025-11-18 17:26:31 +0530

    tested create room functionality

--- src/routes/bookingRoutes.ts
+++ src/routes/bookingRoutes.ts
@@ -7,8 +7,12 @@ bookingRouter.post("/book",(req,res)=>{
     try {
         console.log("the data coming from api request",req.body);
         const result=createBooking(req.body)
+        console.log("the object returned from booking", result);
 
-        res.json(`msg: Your booking was CONFIRMED${result}`)
+        res.json({
+  msg: "Your booking was CONFIRMED",
+  booking: result
+})
     } catch (error) {
         res.json(`msg:your booking was not confirmed ${error}`)
         
