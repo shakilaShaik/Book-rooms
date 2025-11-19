713d688986970e185884c1df0bea1992eae59191
shammu shaik <skshakilask67@gmail.com>
2025-11-19 15:20:21 +0530

    made fixes for cors and added an get api

--- src/data/roomSeed.ts
+++ src/data/roomSeed.ts
@@ -6,6 +6,6 @@ export function seedRooms() {
     rooms.splice(0, rooms.length, 
       { id: "101", name: "Cabin 1", baseHourlyRate: 300, capacity: 4 },
       { id: "102", name: "Conference A", baseHourlyRate: 600, capacity: 12 },
-      { id: "103", name: "Huddle Room", baseHourlyRate: 200, capacity: 2 }
+      { id: "103", name: "small Room", baseHourlyRate: 200, capacity: 2 }
     );
   }
